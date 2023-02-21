8a00b638689160d01347cf855f4ccb7b2d6cf964
cartory <xXcartoryXx@gmail.com>
2023-02-21 00:05:14 -0400

    update penguin > app

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,38 @@ import { Textbox } from "./widgets/textbox/textbox"
 import { Option, icons } from "./widgets/option/option"
 import { Penguin, baloons } from "./widgets/penguin/penguin"
 
+import noseva from "./assets/audio/noseva8bits.mp3"
+import takeOnMe from "./assets/audio/takeOnme8bits.mp3"
+
+// let index = 0
+// const audios = [
+// 	{ name: "No se va by Morat", audio: new Audio(noseva) },
+// 	{ name: "Take On Me by Ellie (TLOU2)", audio: new Audio(takeOnMe) },
+// ]
+
+// const getAudio = () => {
+// 	const music = audios[index++ % 2]
+
+// 	music.audio.load()
+
+// 	music.audio.loop = true
+// 	music.audio.autoplay = true
+// 	music.audio.playbackRate = 1.03
+
+// 	return music
+// }
+
 export const App = () => {
 	const [baloon, setBaloon] = useState(baloons.hello)
 
-	const handleBaloon = (icon) => {
+	const handleBaloon = async (icon) => {
 		const baloonIcon = baloons[icon]
+
+		// if (icon === "play") {
+		// 	console.log("play click")
+		// 	const { audio } = getAudio()
+		// 	await audio.play()
+		// }
 
 		if (!!baloonIcon) {
 			setBaloon(baloonIcon)
