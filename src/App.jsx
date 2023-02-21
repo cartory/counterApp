a5b181f0a212156f9009ef0aa9e0eaf728259689
cartory <xXcartoryXx@gmail.com>
2023-02-20 20:42:46 -0400

    update app > +textbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import "./app.css"
-
 import { useState } from "react"
 
+import { Snow } from "./widgets/snow/snow"
+import { Textbox } from "./widgets/textbox/textbox"
 import { Option, icons } from "./widgets/option/option"
 import { Penguin, baloons } from "./widgets/penguin/penguin"
 
 export const App = () => {
 	const [baloon, setBaloon] = useState(baloons.hello)
 
+	const handleBaloon = (icon) => {
+		const baloonIcon = baloons[icon]
+
+		if (!!baloonIcon) {
+			setBaloon(baloonIcon)
+			document.getElementById("sound").click()
+		}
+
+		return !!baloonIcon
+	}
+
 	return (
-		<div className="app">
-			<Penguin baloon={baloon} setBaloon={setBaloon} />
-			<div className="flex">
-				<div className="textbox">
-					<p>textbox</p>
+		<>
+			<Snow />
+			<div className="app">
+				<Penguin baloon={baloon} setBaloon={setBaloon} />
+				<Textbox />
+				<div className="flex" style={{ gap: 15 }}>
+					{Object.keys(icons).map((icon) => {
+						return <Option key={icon} icon={icons[icon]} onClick={() => handleBaloon(icon)} />
+					})}
 				</div>
 			</div>
-			<div className="flex" style={{ gap: 15 }}>
-				{Object.keys(icons).map((icon) => {
-					return (
-						<Option
-							key={icon}
-							icon={icons[icon]}
-							onClick={() => {
-								const baloonIcon = baloons[icon]
-								
-								if (!!baloonIcon) {
-									setBaloon(baloonIcon)
-									document.getElementById("penguin").click()
-								}
-							}}
-						/>
-					)
-				})}
-			</div>
-		</div>
+		</>
 	)
 }
